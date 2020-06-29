e2a448915d4cf1db96f02415f1fa6030f41e1b73
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-06-28 19:02:04 -0700

    added about section

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const About = () => {
+
+    return (
+        <>
+            <section>
+                <h3>About</h3>
+                <p>Corey Tutle</p>
+                <p>Corey is a graduate student at Whitworth University. He has passion for WWII theology, with a enphasis on all things Detrich Bonnhoefer. Corey is married and has twin toddlers. His passion for bonhoeffer has lead to this Podcast, which is a deep dive into Bonhoeffer Theology with a white range of Bonhoeffer scholars.</p>
+            </section>
+        </>
+    )
+}
